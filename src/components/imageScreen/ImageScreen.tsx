f97fd2f7cc8446b158d89028c377fece5c438ce6
Yulya <suzdlik@gmail.com>
2024-11-30 14:01:13 +0400

    fix image size

--- src/components/imageScreen/ImageScreen.tsx
+++ src/components/imageScreen/ImageScreen.tsx
@@ -7,7 +7,9 @@ export default function ImageScreen(): JSX.Element {
         <section className="image-screen" id="image-screen">
             <div className="container image-screen__wrap">
                 <h2 className="image-screen__title subtitle">Что ждёт за платинкой</h2>
-                <img src={img} alt="photo" className="image-screen__img" />
+                <div className="image-screen__img">
+                    <img src={img} alt="photo" />
+                </div>
                 <p>Межгалактические порты и бесконечные просторы вселенной</p>
             </div>
         </section>
