83578a8b69bb9ad6357f218ce5ba743a461af707
Yulya <suzdlik@gmail.com>
2024-12-02 22:31:51 +0400

    fix flipcard

--- src/components/flipCardsScreen/FlipCard.tsx
+++ src/components/flipCardsScreen/FlipCard.tsx
@@ -9,7 +9,7 @@ export default function FlipCard({flipCard}: FlipCardProps): JSX.Element {
     const [isRotate, setIsRotate] = useState(false);
     const [isShowBackground, setIsShowBackground] = useState(false);
 
-    const onClickHandle = (img: string) => {
+    const onClickHandle = () => {
         setIsRotate(isRotate ? false : true);
     }
 
@@ -23,19 +23,25 @@ export default function FlipCard({flipCard}: FlipCardProps): JSX.Element {
 
     return (
         <div
-            className={isRotate ? 'flip-card rotate' : 'flip-card'}
-            onClick={() => onClickHandle(flipCard.img)}
+            className={isRotate ? 'flip-card__wrap rotate' : 'flip-card__wrap'}
+            onClick={onClickHandle}
             onMouseEnter={onEnterHandle}
             onMouseLeave={onLeaveHandle}
             onTouchStart={onEnterHandle}
             onTouchEnd={onLeaveHandle}
         >
-            <div className="flip-card__wrap">
-                <div className={isShowBackground ? `flip-card__front flip-card__front_bg-${flipCard.id}` : "flip-card__front"}>
-                    <div className="flip-card__title">{flipCard.title}</div>
-                </div>
-                <div className="flip-card__back">
-                    <div className="flip-card__content">{flipCard.content}</div>
+            <div
+                className={isShowBackground ? `flip-card__front flip-card__front_bg-${flipCard.id}` : "flip-card__front"}
+            >
+                <div className="flip-card__title">{flipCard.title}</div>
+            </div>
+            <div className="flip-card__back">
+                <div className="flip-card__content-wrap">
+                    <div
+                        className="flip-card__content"
+                    >
+                        {flipCard.content}
+                    </div>
                 </div>
             </div>
         </div>
